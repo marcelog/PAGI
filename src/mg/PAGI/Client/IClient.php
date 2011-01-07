77e5ae7e9a1264cc8f4e392e2d3875c29e8b9c4f
Marcelo Gornstein <marcelog@gmail.com>
2011-01-07 16:48:31 -0300

    started defining client interface

--- src/mg/PAGI/Client/IClient.php
+++ src/mg/PAGI/Client/IClient.php
@@ -3,5 +3,24 @@ namespace PAGI\Client;
 
 interface IClient
 {
-
+    public function getChannel();
+    public function getLanguage();
+    public function getType();
+    public function getUniqueId();
+    public function getVersion();
+    public function getCallerId();
+    public function getCallerIdName();
+    public function getCallingPres();
+    public function getCallingAni2();
+    public function getCallingTon();
+    public function getCallingTns();
+    public function getDNID();
+    public function getContext();
+    public function getRDNIS();
+    public function getRequest();
+    public function getDNIS();
+    public function getThreadId();
+    public function getAccountCode();
+    public function getEnhanced();
+    public function getPriority();
 }
