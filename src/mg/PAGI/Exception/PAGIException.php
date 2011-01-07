6a0ec698a8180141a7593bd044bcd5fdafdcf18d
Marcelo Gornstein <marcelog@gmail.com>
2011-01-07 14:52:39 -0300

    initial commit

---
+++ src/mg/PAGI/Exception/PAGIException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PAGI family of exceptions.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  Exception
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @version  SVN: $Id$
+ * @link     http://www.noneyet.ar/
+ */
+namespace PAGI\Exception;
+
+/**
+ * PAGI family of exceptions.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  Exception
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @link     http://www.noneyet.ar/
+ */
+class PAGIException extends \Exception
+{
+}
