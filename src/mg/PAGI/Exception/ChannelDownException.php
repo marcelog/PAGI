64115b03a680eb472f8289961ee308d9b7eab313
Marcelo Gornstein <marcelog@gmail.com>
2011-01-08 14:52:12 -0300

    added exceptions related to agi errors

---
+++ src/mg/PAGI/Exception/ChannelDownException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This exception indicates the channel went down suddenly.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  Exception
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @version  SVN: $Id$
+ * @link     http://www.noneyet.ar/
+ */
+namespace PAGI\Exception;
+
+/**
+ * This exception indicates the channel went down suddenly.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  Exception
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @link     http://www.noneyet.ar/
+ */
+class ChannelDownException extends PAGIException
+{
+}
---
+++ src/mg/PAGI/Exception/InvalidCommandException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This exception indicates an invalid agi command.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  Exception
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @version  SVN: $Id$
+ * @link     http://www.noneyet.ar/
+ */
+namespace PAGI\Exception;
+
+/**
+ * This exception indicates an invalid agi command.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  Exception
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @link     http://www.noneyet.ar/
+ */
+class InvalidCommandException extends PAGIException
+{
+}
