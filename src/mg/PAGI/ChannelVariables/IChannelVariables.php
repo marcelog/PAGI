a43b3522a7baf868b84567cff4f9b6573d2c92dc
Marcelo Gornstein <marcelog@gmail.com>
2011-01-10 13:49:35 -0300

    updated doc

--- src/mg/PAGI/ChannelVariables/IChannelVariables.php
+++ src/mg/PAGI/ChannelVariables/IChannelVariables.php
@@ -1,28 +1,184 @@
 <?php
+/**
+ * ChannelVariables facade. Use this to access channel variables.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  ChannelVariables
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @version  SVN: $Id$
+ * @link     http://www.noneyet.ar/
+ */
 namespace PAGI\ChannelVariables;
 
+/**
+ * ChannelVariables facade. Use this to access channel variables.
+ *
+ * PHP Version 5
+ *
+ * @category Pagi
+ * @package  ChannelVariables
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://www.noneyet.ar/ Apache License 2.0
+ * @link     http://www.noneyet.ar/
+ */
 interface IChannelVariables
 {
+    /**
+     * Returns channel (agi_channel).
+     *
+     * @return string
+     */
     public function getChannel();
+
+    /**
+     * Returns language (agi_language).
+     *
+     * @return string
+     */
     public function getLanguage();
+
+    /**
+     * Returns channel type (agi_type).
+     *
+     * @return string
+     */
     public function getType();
+
+    /**
+     * Returns channel uniqueid (agi_uniqueid).
+     *
+     * @return string
+     */
     public function getUniqueId();
+
+    /**
+     * Returns asterisk version (agi_version).
+     *
+     * @return string
+     */
     public function getVersion();
+
+    /**
+     * Returns caller id number (agi_callerid).
+     *
+     * @return string
+     */
     public function getCallerId();
+
+    /**
+     * Returns caller id name (agi_calleridname).
+     *
+     * @return string
+     */
     public function getCallerIdName();
+
+    /**
+     * Returns CallingPres (agi_callingpres).
+     *
+     * @return string
+     */
     public function getCallingPres();
+
+    /**
+     * Returns CallingAni (agi_callingani2).
+     *
+     * @return string
+     */
     public function getCallingAni2();
+
+    /**
+     * Returns CallingTon (agi_callington).
+     *
+     * @return string
+     */
     public function getCallingTon();
+
+    /**
+     * Returns CallingTns (agi_callingtns).
+     *
+     * @return string
+     */
     public function getCallingTns();
+
+    /**
+     * Returns DNID (agi_dnid).
+     *
+     * @return string
+     */
     public function getDNID();
+
+    /**
+     * Returns context (agi_context).
+     *
+     * @return string
+     */
     public function getContext();
+
+    /**
+     * Returns RDNIS (agi_rdnis).
+     *
+     * @return string
+     */
     public function getRDNIS();
+
+    /**
+     * Returns agi requested (agi_request).
+     *
+     * @return string
+     */
     public function getRequest();
+
+    /**
+     * Returns extension dialed (dnis) (agi_extension).
+     *
+     * @return string
+     */
     public function getDNIS();
+
+    /**
+     * Returns thread id (agi_threadid).
+     *
+     * @return string
+     */
     public function getThreadId();
+
+    /**
+     * Returns account code (agi_accountcode).
+     *
+     * @return string
+     */
     public function getAccountCode();
+
+    /**
+     * Returns if using enhanced (agi_enhanced).
+     *
+     * @return string
+     */
     public function getEnhanced();
+
+    /**
+     * Returns context priority (agi_priority).
+     *
+     * @return string
+     */
     public function getPriority();
+
+    /**
+     * Returns total number of agi arguments.
+     *
+     * @return integer
+     */
     public function getTotalArguments();
+
+    /**
+     * Returns the given agi argument. (agi_arg_N).
+     *
+     * @param integer $index Argument number, starting with 0.
+     *
+     * @return string
+     */
     public function getArgument($index);
 }
--- src/mg/PAGI/ChannelVariables/Impl/ChannelVariablesFacade.php
+++ src/mg/PAGI/ChannelVariables/Impl/ChannelVariablesFacade.php
@@ -1,14 +1,60 @@
 <?php
+/**
+ * ChannelVariables facade implementation.
+ *
+ * PHP Version 5
+ *
+ * @category   Pagi
+ * @package    ChannelVariables
+ * @subpackage Impl
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://www.noneyet.ar/
+ */
 namespace PAGI\ChannelVariables\Impl;
 
 use PAGI\ChannelVariables\IChannelVariables;
 
+/**
+ * ChannelVariables facade implementation.
+ *
+ * PHP Version 5
+ *
+ * @category   Pagi
+ * @package    ChannelVariables
+ * @subpackage Impl
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @link       http://www.noneyet.ar/
+ */
 class ChannelVariablesFacade implements IChannelVariables
 {
+    /**
+     * Channel variables given by asterisk.
+     * @var string[]
+     */
     private $_variables;
+
+    /**
+     * AGI Arguments (agi_arg_N).
+     * @var string[]
+     */
     private $_arguments;
+
+    /**
+     * Current instance.
+     * @var ChannelVariablesFacade
+     */
     private static $_instance = false;
 
+    /**
+     * Returns the given variable. Returns false if not set.
+     *
+     * @param string $key Variable to get.
+     *
+     * @return string
+     */
     protected function getAGIVariable($key)
     {
         if (!isset($this->_variables[$key])) {
@@ -17,111 +63,199 @@ class ChannelVariablesFacade implements IChannelVariables
         return $this->_variables[$key];
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getChannel()
+     */
     public function getChannel()
     {
         return $this->getAGIVariable('channel');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getLanguage()
+     */
     public function getLanguage()
     {
         return $this->getAGIVariable('language');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getType()
+     */
     public function getType()
     {
         return $this->getAGIVariable('type');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getUniqueId()
+     */
     public function getUniqueId()
     {
         return $this->getAGIVariable('uniqueid');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getVersion()
+     */
     public function getVersion()
     {
         return $this->getAGIVariable('version');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getCallerId()
+     */
     public function getCallerId()
     {
         return $this->getAGIVariable('callerid');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getCallerIdName()
+     */
     public function getCallerIdName()
     {
         return $this->getAGIVariable('calleridname');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getCallingPres()
+     */
     public function getCallingPres()
     {
         return $this->getAGIVariable('calleridpres');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getCallingAni2()
+     */
     public function getCallingAni2()
     {
         return $this->getAGIVariable('callingani2');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getCallingTon()
+     */
     public function getCallingTon()
     {
         return $this->getAGIVariable('callington');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getCallingTns()
+     */
     public function getCallingTns()
     {
         return $this->getAGIVariable('callingtns');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getDNID()
+     */
     public function getDNID()
     {
         return $this->getAGIVariable('dnid');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getContext()
+     */
     public function getContext()
     {
         return $this->getAGIVariable('context');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getRDNIS()
+     */
     public function getRDNIS()
     {
         return $this->getAGIVariable('rdnis');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getRequest()
+     */
     public function getRequest()
     {
         return $this->getAGIVariable('request');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getDNIS()
+     */
     public function getDNIS()
     {
         return $this->getAGIVariable('extension');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getThreadId()
+     */
     public function getThreadId()
     {
         return $this->getAGIVariable('threadid');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getAccountCode()
+     */
     public function getAccountCode()
     {
         return $this->getAGIVariable('accountcode');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getEnhanced()
+     */
     public function getEnhanced()
     {
         return $this->getAGIVariable('enhanced');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getPriority()
+     */
     public function getPriority()
     {
         return $this->getAGIVariable('priority');
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getTotalArguments()
+     */
     public function getTotalArguments()
     {
         return count($this->_arguments);
     }
 
+    /**
+     * (non-PHPdoc)
+     * @see PAGI\ChannelVariables.IChannelVariables::getArgument()
+     */
     public function getArgument($index)
     {
         if (isset($this->_arguments[$index])) {
@@ -130,6 +264,14 @@ class ChannelVariablesFacade implements IChannelVariables
         return false;
     }
 
+    /**
+     * Returns an instance for this facade implementation.
+     *
+     * @param string[] $variables Initial channel variables given by asterisk.
+     * @param string[] $arguments AGI arguments given by asterisk (agi_arg_N).
+     *
+     * @return ChannelVariablesFacade
+     */
     public static function getInstance(
         array $variables = array(), array $arguments = array()
     ) {
@@ -142,6 +284,14 @@ class ChannelVariablesFacade implements IChannelVariables
         return $ret;
     }
 
+    /**
+     * Constructor.
+     *
+     * @param string[] $variables Initial channel variables given by asterisk.
+     * @param string[] $arguments AGI arguments given by asterisk (agi_arg_N).
+     *
+     * @return void
+     */
     protected function __construct(array $variables, array $arguments)
     {
         $this->_variables = $variables;
