e8e03722f616b85810ad4122e1cf316058480d18
Marcelo Gornstein <marcelog@gmail.com>
2012-01-23 19:01:11 -0300

    build now generates phar and pear packages

--- resources/generatePhar.php
+++ resources/generatePhar.php
@@ -2,11 +2,11 @@
 $stub =
 '<?php
 Phar::mapPhar();
-include "phar://pagi.phar/PAGI/Autoloader/Autoloader.php";
+include "phar://PAGI.phar/PAGI/Autoloader/Autoloader.php";
 \PAGI\Autoloader\Autoloader::register();
 __HALT_COMPILER();
 ?>';
 $phar = new Phar($argv[1]);
-$phar->setAlias('pagi.phar');
+$phar->setAlias('PAGI.phar');
 $phar->buildFromDirectory($argv[2]);
 $phar->setStub($stub);
