499246752230d6a79ca21f8f7a539da8610b9b05
Marcelo Gornstein <marcelog@gmail.com>
2012-05-08 12:09:08 -0300

    added missing file and class comment

--- src/mg/PAGI/Node/NodeActionCommand.php
+++ src/mg/PAGI/Node/NodeActionCommand.php
@@ -1,6 +1,44 @@
 <?php
+/**
+ * A node action command. Basically a DTO to declare what to do when a given
+ * node finished execution.
+ *
+ * PHP Version 5.3
+ *
+ * @category PAGI
+ * @package  Node
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://marcelog.github.com/PAGI/ Apache License 2.0
+ * @version  SVN: $Id$
+ * @link     http://marcelog.github.com/PAGI/
+ *
+ * Copyright 2011 Marcelo Gornstein <marcelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 namespace PAGI\Node;
 
+/**
+ * A node action command. Basically a DTO to declare what to do when a given
+ * node finished execution.
+ *
+ * @category PAGI
+ * @package  Node
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://marcelog.github.com/PAGI/ Apache License 2.0
+ * @link     http://marcelog.github.com/PAGI/
+ */
 class NodeActionCommand
 {
     protected $result = 0;
